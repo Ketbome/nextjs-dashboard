7d2af185955bd253f855056a9e9e850b5ee7d5d4
Ketbome <pablo.moraga@mail.udp.cl>
2024-01-07 12:40:05 -0300

    Add dashboard pages and update fonts
    This commit adds three new dashboard pages: customers, invoices, and the main dashboard page. It also updates the fonts file to include the Lusitana font.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import { Montserrat } from 'next/font/google'
+import { Montserrat, Lusitana } from 'next/font/google'
 
 export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
